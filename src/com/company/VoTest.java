5f084304bd1715e728539ad03014f461dde9dd37
lyy <jankinliu@163.com>
2020-05-23 17:55:15 +0800

    1

---
+++ src/com/company/VoTest.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author lyy
+ * @date 2020/4/1 12:09 上午
+ */
+public class VoTest {
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(3);
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("主线程开始工作");
+        new Thread(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程1完成任务");
+            countDownLatch.countDown();
+        }).start();
+        new Thread(() -> {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程2完成任务");
+            countDownLatch.countDown();
+        }).start();
+        new Thread(() -> {
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程3完成任务");
+            countDownLatch.countDown();
+        }).start();
+        countDownLatch.await();
+        Thread.currentThread().join();
+        System.out.println("全部完成任务，主线程继续执行。。。");
+
+    }
+
+}
