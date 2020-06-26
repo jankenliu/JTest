2571a77bc67f953e79f52c170ef6b31cc9f59b9d
lyy <p_pyyliu@tencent.com>
2020-06-26 16:42:39 +0800

    444

--- src/com/company/VoTest.java
+++ src/com/company/VoTest.java
@@ -21,6 +21,7 @@ public class VoTest {
         System.out.println("我要这个2222");
         System.out.println("主线程开始工作");
         System.out.println("temp=1111111");
+        System.out.println("44444");
         new Thread(() -> {
             try {
                 Thread.sleep(100);
