9614617d2a1e364ae1444710edb763047f5ac78a
lyy <p_pyyliu@tencent.com>
2020-06-26 17:56:29 +0800

    3

--- src/com/company/VoTest.java
+++ src/com/company/VoTest.java
@@ -12,6 +12,7 @@ public class VoTest {
 
     public static void main(String[] args) throws InterruptedException {
 
+        System.out.println("3");
         System.out.println("2");
         System.out.println("11111");
         System.out.println("11111");
